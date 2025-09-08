c41f6d42c1b106019e57b7bbe76722bc5b08e5be
kennycha <dudqn136@naver.com>
2025-09-08 21:31:24 +0900

    fix: export missing types

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,19 @@ import { GraphEditor, type GraphEditorConfig } from "./GraphEditor";
 
 export { GraphEditor };
 export type { GraphEditorConfig };
-export type { Graph, Node, Edge, NodeTypeConfig } from "./types/graph";
+export type {
+  RawGraph,
+  RawNode,
+  Graph,
+  Node,
+  Edge,
+  NodeTypeConfig,
+  NodeType,
+  Position,
+  ViewState,
+  ContextMenuItem,
+  CanvasContextMenuItem,
+  Port,
+} from "./types/graph";
+export type { Theme } from "./types/theme";
+export { defaultTheme } from "./types/theme";
